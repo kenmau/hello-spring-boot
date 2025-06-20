1b4aca2898d7f3fd0e830b50dc9940dfcb9b550c
Ken Mau <ken_mau@hotmail.com>
2025-06-20 07:54:10 -0400

    commit

--- dev-spring-boot/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/km/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev-spring-boot/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/km/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,12 +1,25 @@
 package com.km.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-    // expose "/" that returns "Hello World"
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
 
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team Name: " + teamName;
+    }
+
+    // expose "/" that returns "Hello World"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
